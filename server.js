0c71d92b4b31558de1ea6d72814e60daaf62eb75
dci <rudeshdhote@gmail.com>
2021-10-29 12:14:31 +0200

    changes done

--- server.js
+++ server.js
@@ -4,11 +4,20 @@ import files from "./routes/files.js";
 import show from "./routes/show.js";
 import download from "./routes/download.js";
 import path from "path";
+import cors from "cors";
+
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = dirname(__filename);
+
+// path.dirname(__filename);
+
+// process.cwd();
 
 const app = express();
 connectDB();
 app.use(express.static("public"));
 app.use(express.json());
+app.use(cors());
 
 //Template engine
 const __dirname = path.resolve();
@@ -20,6 +29,10 @@ app.set("view engine", "ejs");
 app.use("/api/files", files);
 app.use("/files", show);
 app.use("/files/download", download);
+
+app.get("/test", (req, res) => {
+  res.send({ msg: "success" });
+});
 
 const PORT = process.env.PORT || 5000;
 
